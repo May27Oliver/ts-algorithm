aa2131cc71780b8c327be0dadc7dd910abc71951
oliver310338 <oliver310338@hotmail.com>
2022-10-20 15:39:34 +0800

    merge sort

--- utils/__test__/sort.test.ts
+++ utils/__test__/sort.test.ts
@@ -1,9 +1,14 @@
-import mergeSort from '../mergeSort';
+import mergeSort from "../mergeSort";
 
-describe('sort', () => {
-    test('mergeSort', () => {
-        const a = 2
-        const b = 3
-        expect(mergeSort(a, b)).toBe(5)
-    })
-})
+describe("sort", () => {
+  test("mergeSort", () => {
+    const x = [4, 7, 1, 2, 5, 3, 8, 10, 33, 6, 77, 100, 101];
+    expect(mergeSort(x)).toEqual([
+      1, 2, 3, 4, 5, 6, 7, 8, 10, 33, 77, 100, 101,
+    ]);
+  });
+  test("mergeSort", () => {
+    const y = [10, 20, 30, 5, 7, 9, 11, 13, 15, 17, 35];
+    expect(mergeSort(y)).toEqual([5, 7, 9, 10, 11, 13, 15, 17, 20, 30, 35]);
+  });
+});
--- utils/mergeSort.ts
+++ utils/mergeSort.ts
@@ -1,3 +1,32 @@
-export default function mergeSort (a: number, b: number): number {
-    return a + b
+export default function mergeSort(arr: number[]): number[] {
+  if (arr.length < 2) {
+    return arr;
+  }
+  const midIdx = arr.length / 2;
+  const arrLeft = mergeSort(arr.slice(0, midIdx));
+  const arrRight = mergeSort(arr.slice(midIdx, arr.length));
+
+  return merge(arrLeft, arrRight);
 }
+
+const merge = (left: number[], right: number[]): number[] => {
+  let finalArr: number[] = [];
+  let i = 0,
+    j = 0;
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      finalArr = [...finalArr, left[i]];
+      i++;
+    } else {
+      finalArr = [...finalArr, right[j]];
+      j++;
+    }
+  }
+  for (; i < left.length; i++) {
+    finalArr = [...finalArr, left[i]];
+  }
+  for (; j < right.length; j++) {
+    finalArr = [...finalArr, right[j]];
+  }
+  return finalArr;
+};
