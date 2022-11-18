2f6a2eb8c7a5bd35e3716cd3278848fbed5691b6
oliver310338 <oliver310338@hotmail.com>
2022-11-18 08:51:58 +0800

    LeetCode 67. Add Binary

--- utils/AddBinary.ts
+++ utils/AddBinary.ts
@@ -1,7 +1,61 @@
 /*
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 Increment the large integer by one and return the resulting array of digits.
+1.原本想把字串轉成數字相加，但會發現數字可能會超過Number的範圍。
+2.先把字串reverse，對齊兩字串的個位數字，跑迴圈相加
+3.設定一個變數carry處理進位問題。
+4.先處理長短字串相加
+5.處理餘下長字串進位
+
+reference:
+https://skyyen999.gitbooks.io/-leetcode-with-javascript/content/questions/67md.html
 */
-export default function plusOne(digits: number[]): number[] {
-  return [0];
+export default function addBinary(a: string, b: string): string {
+  let longStr: string,
+    shortStr: string,
+    sumString: string = "";
+
+  if (a.length > b.length) {
+    longStr = a;
+    shortStr = b;
+  } else {
+    longStr = b;
+    shortStr = a;
+  }
+  let carry = 0;
+
+  //對齊兩個字串
+  longStr = longStr.split("").reverse().join("");
+  shortStr = shortStr.split("").reverse().join("");
+
+  //處理長短陣列相加
+  for (let i = 0; i < shortStr.length; i++) {
+    let num =
+      parseInt(shortStr.charAt(i)) + parseInt(longStr.charAt(i)) + carry;
+    if (num > 1) {
+      //大於一就進位
+      carry = 1;
+      num = num % 2;
+    } else {
+      carry = 0;
+    }
+    sumString = num.toString() + sumString;
+  }
+
+  //處理餘下的長陣列進位
+  for (let j = shortStr.length; j < longStr.length; j++) {
+    let num = parseInt(longStr.charAt(j)) + carry;
+    if (num > 1) {
+      carry = 1;
+      num = num % 2;
+    } else {
+      carry = 0;
+    }
+    sumString = num.toString() + sumString;
+  }
+  if (carry === 1) {
+    return "1" + sumString;
+  } else {
+    return sumString;
+  }
 }
--- utils/__test__/leetcode.test.ts
+++ utils/__test__/leetcode.test.ts
@@ -8,6 +8,7 @@ import linkedList from "../../data_structure/linkedList";
 import lengthOfLastWord from "../LengthOfLastWord";
 import SearchInsertPosition from "../SearchInsertPosition";
 import PlustOne from "../PlusOne";
+import AddBinary from "../AddBinary";
 
 describe("leetcode test", () => {
   test("twoSum test1", () => {
@@ -155,5 +156,32 @@ describe("leetcode test", () => {
     expect(PlustOne(digits)).toEqual([
       6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 4,
     ]);
+  });
+  /*
+    Input: a = "11", b = "1"
+    Output: "100"
+  */
+  test("Add Binary 1", () => {
+    const a = "11",
+      b = "1";
+    expect(AddBinary(a, b)).toBe("100");
+  });
+  /*
+    Input: a = "1010", b = "1011"
+    Output: "10101"
+  */
+  test("Add Binary 2", () => {
+    const a = "1010",
+      b = "1011";
+    expect(AddBinary(a, b)).toBe("10101");
+  });
+  test("Add Binary 3", () => {
+    const a =
+        "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
+      b =
+        "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+    expect(AddBinary(a, b)).toBe(
+      "110111101100010011000101110110100000011101000101011001000011011000001100011110011010010011000000000"
+    );
   });
 });
