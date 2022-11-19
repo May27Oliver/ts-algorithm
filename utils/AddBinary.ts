bff3516f57470a7c110aca41fccc0d785e7aaa0e
oliver310338 <oliver310338@hotmail.com>
2022-11-20 00:24:30 +0800

    Add Binary refractor

--- utils/AddBinary.ts
+++ utils/AddBinary.ts
@@ -1,14 +1,6 @@
 /*
 You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 Increment the large integer by one and return the resulting array of digits.
-1.原本想把字串轉成數字相加，但會發現數字可能會超過Number的範圍。
-2.先把字串reverse，對齊兩字串的個位數字，跑迴圈相加
-3.設定一個變數carry處理進位問題。
-4.先處理長短字串相加
-5.處理餘下長字串進位
-
-reference:
-https://skyyen999.gitbooks.io/-leetcode-with-javascript/content/questions/67md.html
 */
 //Better one
 export default function addBinary(a: string, b: string): string {
