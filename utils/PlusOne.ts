4d58c58f9ca6e041df2f9a639633c4ffdf38d19e
oliver310338 <oliver310338@hotmail.com>
2022-11-20 10:38:33 +0800

    PlusOne refractor

--- utils/PlusOne.ts
+++ utils/PlusOne.ts
@@ -2,32 +2,53 @@
 Given two binary strings a and b, return their sum as a binary string.
 */
 export default function plusOne(digits: number[]): number[] {
-  /*
-    1. 檢查最後一個值，加一，
-    2. 如果遇到9，在下個位置繼續加一
-  */
-  let carry = false;
-  for (let i = digits.length - 1; i >= 0; i--) {
-    let num = digits[i];
-    if (i === digits.length - 1 && num === 9) {
+  let carry: number = 1,
+    i: number = digits.length - 1;
+
+  while (i >= 0) {
+    carry = carry + digits[i];
+    if (carry > 9) {
       digits[i] = 0;
-      carry = true;
-      continue;
-    } else if (i === digits.length - 1 && num < 9) {
-      digits[i] = digits[i] + 1;
-      carry = false;
-      continue;
+      carry = 1;
+    } else {
+      digits[i] = carry;
+      carry = 0;
     }
-    if (carry && num < 9) {
-      digits[i] = digits[i] + 1;
-      carry = false;
-    } else if (carry && num === 9) {
-      digits[i] = 0;
-      carry = true;
-    }
+    i--;
   }
-  if (carry) {
+  if (carry === 1) {
     digits = [1, ...digits];
   }
   return digits;
 }
+
+// export default function plusOne(digits: number[]): number[] {
+//   /*
+//     1. 檢查最後一個值，加一，
+//     2. 如果遇到9，在下個位置繼續加一
+//   */
+//   let carry = false;
+//   for (let i = digits.length - 1; i >= 0; i--) {
+//     let num = digits[i];
+//     if (i === digits.length - 1 && num === 9) {
+//       digits[i] = 0;
+//       carry = true;
+//       continue;
+//     } else if (i === digits.length - 1 && num < 9) {
+//       digits[i] = digits[i] + 1;
+//       carry = false;
+//       continue;
+//     }
+//     if (carry && num < 9) {
+//       digits[i] = digits[i] + 1;
+//       carry = false;
+//     } else if (carry && num === 9) {
+//       digits[i] = 0;
+//       carry = true;
+//     }
+//   }
+//   if (carry) {
+//     digits = [1, ...digits];
+//   }
+//   return digits;
+// }
