2a35959bd3b47127ff83c4da7e33ca8c6f15b2aa
oliver310338 <oliver310338@hotmail.com>
2022-12-10 15:20:08 +0800

    array build treee

--- utils/arrayBuildTree.ts
+++ utils/arrayBuildTree.ts
@@ -4,26 +4,16 @@ export default function arrayBuildTreeByLevelTraveral(arr: (number | null)[]) {
   const treeArr = arr.map((item) =>
     typeof item === "number" ? new TreeNode(item) : null
   );
-  const addLeftTreeNode = (i: number): void => {
+  const addTreeNode = (i: number): void => {
     const node = treeArr[i];
     if (!node) {
       return;
     }
     node.left = treeArr[2 * i + 1];
-    addLeftTreeNode(2 * i + 1);
-    addLeftTreeNode(2 * i + 2);
+    node.right = treeArr[2 * i + 2];
+    addTreeNode(2 * i + 1);
+    addTreeNode(2 * i + 2);
   };
-  const addRightTreeNode = (i: number): void => {
-    const node = treeArr[i];
-    if (!node) {
-      return;
-    } else {
-      node.right = treeArr[2 * i + 2];
-      addRightTreeNode(2 * i + 1);
-      addRightTreeNode(2 * i + 2);
-    }
-  };
-  addLeftTreeNode(0);
-  addRightTreeNode(0);
+  addTreeNode(0);
   return treeArr[0];
 }
